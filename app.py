bb83b62ee38bca72bb134ea51f5df10d1e5abd97
soybean-dou <iu98@naver.com>
2023-07-28 17:00:57 +0900

    add bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+# app.py
+from flask import Flask, render_template, request
 
+#Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=('GET','POST')) # 접속하는 url
 def index():
-  return render_template('index.html')
+    if request.method == "POST":
+        print(request.form.get('user'))
+        user = request.form.get('user')
+        return render_template('index.html',user=user)
+    elif request.method == "GET":
+       user="반원"
+       return render_template('index.html',user=user)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__=="__main__":
+  app.run(debug=True)
+  # host 등을 직접 지정하고 싶다면
+  # app.run(host="127.0.0.1", port="5000", debug=True)
