e7e39aad835455d77b98c8424572965eabd5f5c6
soybean-dou <iu98@naver.com>
2023-08-01 18:46:45 +0900

    add new design

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-# app.py
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask_bootstrap import Bootstrap
+import os
 
-#Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-@app.route('/',methods=('GET','POST')) # 접속하는 url
+UPLOAD_FOLDER = '~/pneumo_page/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
 def index():
-    if request.method == "POST":
-        print(request.form.get('user'))
-        user = request.form.get('user')
-        return render_template('index.html',user=user)
-    elif request.method == "GET":
-       user="반원"
-       return render_template('index.html',user=user)
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(file.filename)
+            return '업로드 성공'
+    return render_template('submit.html')
 
 
-if __name__=="__main__":
-  app.run(debug=True)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ static/js/submit.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 업로드 버튼 클릭 이벤트 리스너 등록
+})
